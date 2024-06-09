f1f641c08b093783e129964ffa0b17fb34a53e98
Aleksandr Zapparov <alexander@zapparov.dev>
2024-06-09 17:07:40 +0200

    Update flask run and index.html template
    Update to flask run so it can restart when changes are made to files. Update index.html to have a proper header and started out on the form. Added alpinejs to dependencies.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
