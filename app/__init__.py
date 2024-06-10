a06b46c4c5f26757dfab65802f67a580e88e34b3
Aleksandr Zapparov <alexander@zapparov.dev>
2024-06-10 10:15:18 +0200

    Update frontend post request and backend to receive and store image

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,30 @@
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
-
 import os
+from flask import Flask, flash, request, redirect, render_template, url_for
+from pprint import pp
+from werkzeug.utils import secure_filename
+
+# import abort
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "app/static/images/uploads")
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config["IMAGE_UPLOADS"] = "app/static/images/uploads"
-app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
-app.config['MAX_CONTENT_LENGTH'] = 10240
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 15 * 1024 * 1024
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/stash", methods=["POST"])
-def stash():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            if image.filename == "":
-                print("No filename")
-                return redirect(request.url)
-            if allowed_image(image.filename):
-                image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-                print("Image saved")
-                return redirect(request.url)
-            else:
-                print("That file extension is not allowed")
-                return redirect(request.url)
 
-    return render_template("index.html")
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    data = request.files['file']
+    data.save(os.path.join(app.config['UPLOAD_FOLDER'], data.filename))
+    pp(data)
+    return '', 204
